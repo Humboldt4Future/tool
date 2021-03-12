0c1c161bd6a8500305524601abcd5344d497a24f
Ph4nt4sm4 <73649872+Ph4nt4sm4@users.noreply.github.com>
2021-03-12 15:51:28 +0100

    fixed a missing file

---
+++ src/styles.js
@@ -0,0 +1,21 @@
+import { makeStyles } from '@material-ui/core';
+
+export default makeStyles(() => ({
+    appBar: {
+        borderRadius: 15,
+        margin: '30px 0',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    heading: {
+        color: 'rgba(0,118,55,99)',
+    },
+    image: {
+        marginLeft: '15px',
+    },
+    mainContainer: {
+        flexDirection: "column-reverse",
+    }
+}));
